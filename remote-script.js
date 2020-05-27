70a6b28f82db6b3b5440886ef671e050fa315c54
Jeff Heppert <jeff@august.com>
2020-05-27 15:23:25 -0700

    added initial qr code making stuff

--- remote-script.js
+++ remote-script.js
@@ -6,12 +6,32 @@
             var currentHostname = window.location.hostname;
             if(currentHostname.endsWith('august.com')) {
                 var serialNumber = document.getElementById("identifier").value;
-                var codeText = "AUG:" + serialNumber;
+                var codeText = "";
+                if (serialNumber != "") {
+                    codeText = "AUG:" + serialNumber;
+                }
                 console.log(codeText);
+
+                //Load qrcode.js library
                 var j = document.createElement('script');
                 j.src = '//jheppert.github.io/qr-code-tools/qrcode.js';
                 j.onload = function() {
                     console.log("in the callback function");
+                    var codeContents = document.getElementById("qrcode");
+
+                    // Making the QR Code
+                    var qrcode = new QRCode(codeContents, {
+                        width : 200,
+                        height : 200
+                    });
+                    
+                    if (codeText != "") {
+                        qrcode.makeCode(codeText);
+                    } else {
+                        console.log("Didn't find a serial number");
+                        return;
+                    }
+
                 }
                 document.body.appendChild(j);
             } else {
